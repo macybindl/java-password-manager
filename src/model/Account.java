5bc46d33fcb6fdcfd800691ad9d8a862f5a04e1c
macybindl <bindl9370@uwlax.edu>
2025-11-12 12:30:44 -0600

    Account class implemented

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Account {
+    private String site;
+    private String username;
+    private String encryptedPassword;
+
+    public Account(String site, String username, String encryptedPassword) {
+        this.site = site;
+        this.username = username;
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getSite() {
+        return site;
+    }
     
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Site: " + site + ", Username: " + username;
+    }
 }

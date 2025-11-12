6e6f9a880b475f0561414fab9aa8c3cb013ad663
macybindl <bindl9370@uwlax.edu>
2025-11-12 13:25:31 -0600

    Implement Encryption

--- src/util/Encryption.java
+++ src/util/Encryption.java
@@ -1,5 +1,27 @@
 package util;
 
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Encryption {
     
+    // 16-character key (AES - 128)
+    private static final String SECRET_KEY = "1234567890123456";
+    
+    // Encrypts a plain text string using AES
+    public static String encrypt(String strToEncrypt) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+    }
+
+    // Decrypts an AES-encrypted Base64 string
+    public static String decrypt(String strToDecrypt) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+    }
 }
